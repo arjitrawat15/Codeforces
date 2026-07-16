3930f5df1b499a3ebf1f24f0f60a86273745f9c4
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-07-16 23:56:03 +0530

    add 1108 contest code

---
+++ A_farmpiggie_and_Subset_Sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            int value=(i & 1) ? i+1:i-1;
+            cout<<value<<(i==n? '\n':' ');
+        }
+    }
+    return 0;
+}
