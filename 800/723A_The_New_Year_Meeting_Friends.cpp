db0bdec0e6c267104c3c35e07463c40151544aaa
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-07-24 17:43:58 +0530

    Add code for 723A problem in codeforces

---
+++ 800/723A_The_New_Year_Meeting_Friends.cpp
@@ -0,0 +1,13 @@
+//Problem 723A The New Year Meeting Friends
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    vector<int>arr(3);
+    cin>>arr[0]>>arr[1]>>arr[2];
+    sort(arr.begin(),arr.end());
+    cout<<(arr[1]-arr[0]) + (arr[2]-arr[1]);
+    return 0;
+}
