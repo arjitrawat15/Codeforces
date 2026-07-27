d9be29a84b9aa03128689e3615e0ed9efaeec79b
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-07-27 22:44:58 +0530

    Add code for 1878A problem in codeforces

---
+++ 800/1878A_How_Much_Does_Daytona_Cost.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }    
+        bool found=false;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==k)
+            {
+                found=true;
+            }
+        }
+        if(found)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
