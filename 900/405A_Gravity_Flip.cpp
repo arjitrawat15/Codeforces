d1835acbba95ab65fe1e9dfd9a28f34e6163f81e
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-09-19 00:35:37 +0530

    Add code for 405A problem in codeforces

---
+++ 900/405A_Gravity_Flip.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    for(auto it:arr)
+    {
+        cout<<it<<" ";
+    }
+    return 0;
+}
