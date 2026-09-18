51f3fcff9e269b50b356201a274816208636c0fc
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-09-18 23:49:02 +0530

    Add code for 96A problem in codeforces

---
+++ 900/96A_Football.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    string s;
+    cin>>s; 
+    int cnt=1;
+    for(int i=1;i<s.size();i++) 
+    {
+        if(s[i]==s[i-1]) 
+        {
+            cnt++;
+            if(cnt>=7) 
+            {
+                cout<<"YES";
+                return 0;
+            }
+        } 
+        else 
+        {
+            cnt=1;
+        }
+    }
+    cout<<"NO";
+    return 0;
+}
